e969627d766bb878b0cc3a41c3a6c7872309974b
Andrzej Rumak <ajgltl@gmail.com>
2024-10-15 23:34:59 +0200

    First frog

---
+++ _nuxt/CKw2HztT.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./EpcTT5XR.js";import"./CASETPd-.js";export{m as default};
---
+++ _nuxt/hzpy_QpB.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./r2zUDdNq.js";import{_ as r,y as a,A as s,o as c,w as _,b as l,a as p}from"./CASETPd-.js";import"./CkvVkR0O.js";const f={};function m(i,o){const e=a("Placeholder"),t=n;return c(),s(t,null,{default:_(()=>[l(e,{class:"h-32"}),o[0]||(o[0]=p("h1",null,"My Profile",-1))]),_:1})}const h=r(f,[["render",m]]);export{h as default};
